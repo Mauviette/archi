fdabbc6473cbf7fa3046c7f38ec67746a82e0708
bileluwu <mbchriki@gmail.com>
2025-03-12 16:06:41 +0100

    frame

--- src/vue/FramePrincipale.java
+++ src/vue/FramePrincipale.java
@@ -1,7 +1,33 @@
 package vue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import controleur.Controleur;
 
 public class FramePrincipale extends JFrame {
+	private Controleur ctrl;
+	private JPanel panel;
 
+	public FramePrincipale(Controleur ctrl) {
+		this.ctrl=ctrl;
+
+		this.setTitle("Les couleurs tt ça");
+		this.setSize(800, 800);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+		this.panel = new JPanel();
+		panel.add(new JLabel(new ImageIcon("./img/img.jpg")));
+
+		this.add(panel);
+
+        this.setVisible(true);
+	}
+	public static void main(String[] args) {
+		Controleur ctrl = new Controleur();
+		FramePrincipale fr = new FramePrincipale(ctrl);
+	}
 }
